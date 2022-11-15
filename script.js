44f776fe0f7c99951278ae40fe1d0ed49c167edd
Abdul703 <63172951+Abdul703@users.noreply.github.com>
2022-11-15 11:04:07 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+function next(change){
+  const images = ['image1', 'image2', 'image3', 'image4', 'image5', 'image6'];
+  let newNum = 0;
+  
+  let imagePath = document.getElementById('img').src;
+  let len = imagePath.length;
+  let imageName = imagePath.slice(len-10, len-4);
+  let num = images.indexOf(imageName);
+  
+  if (change == "nxt"){
+    newNum = num + 1;
+  }else{
+    if (num == 0){
+      newNum = 5;
+    }else{
+      newNum = num - 1;
+    }
+  }
+  
+  let newPath = imagePath.slice(0, len-10) + images[newNum%6] + ".jpg";
+  
+  document.getElementById('img').src = newPath;
+  console.log(newPath);
+}
