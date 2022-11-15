7a634637fe1ba69e36e57204e814b6c1e9c6833d
Abdulazeez Nuhu <nuhuabdul86@gmail.com>
2022-11-15 23:41:05 +0100

    Created a folder Images and move all the images to it

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function next(change){
+function change(direction){
   const images = ['image1', 'image2', 'image3', 'image4', 'image5', 'image6'];
   let newNum = 0;
   
@@ -7,7 +7,7 @@ function next(change){
   let imageName = imagePath.slice(len-10, len-4);
   let num = images.indexOf(imageName);
   
-  if (change == "nxt"){
+  if (direction == "nxt"){
     newNum = num + 1;
   }else{
     if (num == 0){
@@ -20,5 +20,4 @@ function next(change){
   let newPath = imagePath.slice(0, len-10) + images[newNum%6] + ".jpg";
   
   document.getElementById('img').src = newPath;
-  console.log(newPath);
 }
